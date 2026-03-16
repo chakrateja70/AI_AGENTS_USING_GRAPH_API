bf7842564a5ed39d3ff357117cb1620d850a7afd
teja <tejapolamarasetty2004@gmail.com>
2026-03-16 20:59:26 +0530

    added gemini llm integration

---
+++ llm_gemini.py
@@ -0,0 +1,44 @@
+# To run this code you need to install the following dependencies:
+# pip install google-genai
+
+import os
+from google import genai
+from google.genai import types
+
+
+def generate():
+    client = genai.Client(
+        api_key="AIzaSyC4Y3rlKkNjRqanzqpvEyC9v3w0LeCgcuo",
+    )
+
+    model = "gemini-2.5-flash"
+    contents = [
+        types.Content(
+            role="user",
+            parts=[
+                types.Part.from_text(text="what is llm"),
+            ],
+        ),
+    ]
+    tools = [
+        types.Tool(googleSearch=types.GoogleSearch(
+        )),
+    ]
+    generate_content_config = types.GenerateContentConfig(
+        thinking_config=types.ThinkingConfig(
+            include_thoughts=True,
+        ),
+        tools=tools,
+    )
+
+    for chunk in client.models.generate_content_stream(
+        model=model,
+        contents=contents,
+        config=generate_content_config,
+    ):
+        print(chunk.text, end="")
+
+if __name__ == "__main__":
+    generate()
+
+
